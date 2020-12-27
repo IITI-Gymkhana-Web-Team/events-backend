a8ffbb5a9b1329ce040039c10aade48d22f24ee9
= <garvitgalgat@gmail.com>
2020-12-27 14:48:16 +0530

    update events

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, render_template, request
 from flask_mysqldb import MySQL
 import MySQLdb
 import yaml
 import os
 from dbConfig import database_config
+from test import get_commands
 
 app = Flask(__name__)
 
-env = ""
+env = "dev"
 
 if env == "dev":
     dev = yaml.load(open('db.yaml'), Loader=yaml.FullLoader)
-    DATABASE_URL  = dev['CLEARDB_DATABASE_URL']
-    PASSWORD  = dev['PASSWORD']
+    DATABASE_URL = dev['CLEARDB_DATABASE_URL']
+    PASSWORD = dev['PASSWORD']
 
 else:
-    DATABASE_URL  = os.environ.get('CLEARDB_DATABASE_URL')
-    PASSWORD  = os.environ.get('PASSWORD')
+    DATABASE_URL = os.environ.get('CLEARDB_DATABASE_URL')
+    PASSWORD = os.environ.get('PASSWORD')
 
 user, password, host, db = database_config(DATABASE_URL)
 
@@ -32,71 +33,118 @@ if env == 'dev':
 else:
     app.SECRET_KEY = os.environ.get("SECRET_KEY")
 
-def get_events(table_name):
+
+def get_events(table_name, for_editing=False):
     cur = mysql.connection.cursor()
     cur.execute('select * from ' + table_name + ';')
-    events = cur.fetchall()  
+    events = cur.fetchall()
     print(len(events))
-    
+
     event_list = []
     for i in events:
         obj = {
-            "title": i[0],
-            "description": i[1],
-            "details": i[2],
-            "date-of-event": i[3],
-            "image": i[4]
+            "id": i[0],
+            "title": i[1],
+            "description": i[2],
+            "details": i[3],
+            "date-of-event": i[4],
+            "image": i[5]
         }
         event_list.append(obj)
     response_body = {
-    "size": len(events),
-    "events": event_list
-}
+        "size": len(events),
+        "events": event_list
+    }
+    if(for_editing == True):
+        return response_body
+
     res = make_response(jsonify(response_body), 200)
     return res
+
 
 @app.route('/')
 def example():
     response_body = {
-    "size": 3,
-    "events": [
-        {
-            "title": "This is an example",
-            "description": "Example event",
-            "details": "More details about the event",
-            "date-of-event": "Date in YYYY-MM-DD format",
-            "image": "image url of the link"
-        },
-        {
-            "title": "TItle of event",
-            "description": "Short description of the event",
-            "details": "More details about the event",
-            "date-of-event": "Date in YYYY-MM-DD format",
-            "image": "image url of the link"
-        },
-        {
-            "title": "TItle of event",
-            "description": "Short description of the event",
-            "details": "More details about the event",
-            "date-of-event": "Date in YYYY-MM-DD format",
-            "image": "image url of the link"
-        }, 
-    ]
-}
+        "size": 3,
+        "events": [
+            {
+                "id": 1,
+                "title": "TItle of event",
+                "description": "Short description of the event",
+                "details": "More details about the event",
+                "date-of-event": "Date in YYYY-MM-DD format",
+                "image": "image url of the link"
+            },
+            {
+                "id": 2,
+                "title": "TItle of event",
+                "description": "Short description of the event",
+                "details": "More details about the event",
+                "date-of-event": "Date in YYYY-MM-DD format",
+                "image": "image url of the link"
+            },
+            {
+                "id": 3,
+                "title": "TItle of event",
+                "description": "Short description of the event",
+                "details": "More details about the event",
+                "date-of-event": "Date in YYYY-MM-DD format",
+                "image": "image url of the link"
+            },
+        ]
+    }
     res = make_response(jsonify(response_body), 200)
     return res
 
+
 @app.route('/past')
 def past():
-    return get_events("past_event")
+    return get_events("past_events")
+
 
 @app.route('/ongoing')
 def ongoing():
     return get_events('ongoing_events')
 
+
 @app.route('/upcoming')
 def upcoming():
     return get_events('upcoming_events')
+
+
+@app.route('/<event_time>/edit')
+def edit(event_time):
+    if(event_time == "past" or event_time == "ongoing" or event_time == "upcoming"):
+        table_name = event_time + "_events"
+        res = get_events(table_name, True)
+        events = res['events']
+        size = res['size']
+        return render_template('editor.html', size=size, events=events, event_time=event_time.capitalize(), table_name=table_name)
+    else:
+        return "Wrong Parameter Supplied"
+
+
+@app.route("/update", methods=['POST'])
+def update():
+    data = request.form.to_dict()
+    if(data['password'] == PASSWORD):
+        commands = get_commands(data, data['table_name'])
+
+        cur = mysql.connection.cursor()
+
+        for i in commands:
+            try:
+                print("Executing: " + i)
+                cur.execute(i)
+            except (MySQLdb.Error, MySQLdb.Warning) as e:
+                print(e)
+
+        mysql.connection.commit()
+        cur.close()
+
+        return "Successfully Updated"
+    else:
+        return "Incorrect Password"
 
 
 if __name__ == "__main__":
---
+++ test.py
@@ -0,0 +1,36 @@
+# returns a list of commands to be executed
+def get_commands(data, table_name):
+
+    x = data
+    #  Example -------
+    # {
+    #     "32": ["title", "description", "details", "20-11-10", "image_url"],
+    #     "12": ["title", "description", "details", "20-11-10", "image_url"]
+    # }
+    obj = {}
+    for item in x:
+        if '-' in item:
+            [data, id] = item.split('-')
+            if id not in obj:
+                obj[id] = [0, 0, 0, 0, 0]
+            if(data == 'title'):
+                obj[id][0] = x[item]
+            if(data == 'description'):
+                obj[id][1] = x[item]
+            if(data == 'details'):
+                obj[id][2] = x[item]
+            if(data == 'date'):
+                obj[id][3] = x[item]
+            if(data == 'image'):
+                obj[id][4] = x[item]
+            # print("UPDATE TABLE SET {} = '{}' WHERE ID = {};".format(data,x[item], id))
+    cols = ['title', 'description', 'details', 'date_of_event', 'image']
+    commands = []
+    for id in obj:
+        commands.append("UPDATE {} SET {} = '{}', {} = '{}', {} = '{}', {} = '{}', {} = '{}' WHERE ID = {};".format(
+            table_name, cols[0], obj[id][0], cols[1], obj[id][1], cols[2], obj[id][2], cols[3], obj[id][3], cols[4], obj[id][4], id
+        ))
+
+    return commands
+
+    # print(obj)
