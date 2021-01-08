8fcb70d9cffcd29485525f4371249aa5b1f12800
= <garvitgalgat@gmail.com>
2021-01-08 23:06:17 +0530

    env changed from dev

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-env = "dev"
+env = ""
 
 if env == "dev":
     dev = yaml.load(open('db.yaml'), Loader=yaml.FullLoader)
